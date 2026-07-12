2d3e66c02c6dece58161992edcc7fbb7fbc5b1d7
9amLeads <hello@9amleads.com>
2026-07-12 14:41:17 +0100

    Add dirname and path info to stripe diagnostic

--- mission control/production_api_server.js
+++ mission control/production_api_server.js
@@ -7849,12 +7849,20 @@ app.get('/api/admin/check-stripe', adminAuth, (req, res) => {
   var keyPrefix = STRIPE_SECRET_KEY ? STRIPE_SECRET_KEY.substring(0, 10) + '...' : '';
   var priceIdsCount = Object.keys(STRIPE_PRICE_IDS).length;
   var newbusinessPrices = STRIPE_PRICE_IDS['newbusiness'] || {};
+  var configPath = path.join(DATA_DIR, 'stripe-config.json');
+  var altConfigPath = path.join(__dirname, 'data', 'stripe-config.json');
   res.json({
     hasSecretKey: hasKey,
     keyPrefix: keyPrefix,
     priceIdProducts: Object.keys(STRIPE_PRICE_IDS),
     newbusinessPrices: newbusinessPrices,
-    configFileExists: fs.existsSync(path.join(DATA_DIR, 'stripe-config.json'))
+    configFileExists: fs.existsSync(configPath),
+    altConfigExists: fs.existsSync(altConfigPath),
+    dirname: __dirname,
+    dataDir: DATA_DIR,
+    configPath: configPath,
+    altConfigPath: altConfigPath,
+    cwd: process.cwd()
   });
 });
 app.post('/api/admin/clear-leads', adminAuth, (req, res) => {
